9461477d22cae8a9f3f92cfcc2796f8591abe1ac
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-01 10:41:28 +0100

    updated js

---
+++ script.js
@@ -0,0 +1,21 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+let displayTime = document.getElementById("time");
+let timer = null;
+
+function stopWatch() {
+  seconds++;
+  if (seconds === 60) {
+    seconds = 0;
+    minutes++;
+    if (minutes === 60) {
+      minutes = 0;
+      hours++;
+    }
+  }
+}
+function watchStart() {
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(stopWatch, 1000);
+}
