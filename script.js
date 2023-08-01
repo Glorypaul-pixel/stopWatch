992c29b549d0fd0b5aa482a5c4a6b50e3dce0342
Glorypaul-pixel <ifeomaglory1998@gmail.com>
2023-08-01 11:57:58 +0100

    i think i am done

--- script.js
+++ script.js
@@ -4,18 +4,30 @@ let timer = null;
 
 function stopWatch() {
   seconds++;
-  if (seconds === 60) {
+  if (seconds == 60) {
     seconds = 0;
     minutes++;
-    if (minutes === 60) {
+    if (minutes == 60) {
       minutes = 0;
       hours++;
     }
   }
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  displayTime.innerHTML = "<h1>" + h + ":" + m + ":" + s + "</h1>";
 }
 function watchStart() {
   if (timer !== null) {
     clearInterval(timer);
   }
   timer = setInterval(stopWatch, 1000);
+}
+function watchStop() {
+  clearInterval(timer);
+}
+function watchReverse() {
+  clearInterval(timer);
+  [seconds, minutes, hours] = [0, 0, 0];
+  displayTime.innerHTML = "00:00:00";
 }
